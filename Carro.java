ed64eb722384f10f6d753d27c667e8e5dbcf8f99
vlecabr <vitartasleca@gmail.com>
2025-05-13 23:14:00 -0300

    comentando codigos

--- Carro.java
+++ Carro.java
@@ -1,14 +1,19 @@
 public class Carro {
+    // Atributos
     String modelo;
     int ano;
     String cor;
 
+    // Método construtor
+    // O construtor exibe os atributos do carro
     void exibirDetalhes() {
         System.out.println("Modelo: " + modelo);
         System.out.println("Ano: " + ano);
         System.out.println("Cor: " + cor);
     }
 
+    // Método para calcular a idade do carro
+    // A idade é calculada subtraindo o ano atual do ano do carro
     void calcularIdade(){
         int anoAtual = 2025;
         int idade = anoAtual - ano;
