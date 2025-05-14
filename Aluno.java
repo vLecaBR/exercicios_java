92c6cff76d32e030891359bda6c95f580897c196
vlecabr <vitartasleca@gmail.com>
2025-05-13 23:09:55 -0300

    comentando codigos

--- Aluno.java
+++ Aluno.java
@@ -1,14 +1,15 @@
 
-
+// Nome do arquivo: Aluno.java
 import java.util.Scanner;
 
-
+// Classe Aluno
+// Atributos: nome e idade
 public class Aluno {
     Scanner scanner = new Scanner(System.in);
-
+    // Atributos
     String nome;
     int idade;
-
+    // Metodo construtor
     void exibirDetalhes(){
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
