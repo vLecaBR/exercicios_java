d6ae568871af2635a8a8cfadba1354b2469e615a
vlecabr <vitartasleca@gmail.com>
2025-05-13 23:15:17 -0300

    comentando codigos

--- MainCarro.java
+++ MainCarro.java
@@ -1,11 +1,16 @@
 public class MainCarro {
         public static void main(String[] args) {
+
+        // Cria um objeto da classe Carro
         Carro carro1 = new Carro();
 
+        // Atribui valores aos atributos do objeto carro1
         carro1.modelo = "Civic";
         carro1.ano = 1998;
         carro1.cor = "Preto";
         
+        // Chama o método exibirDetalhes para exibir os detalhes do carro
+        // Chama o método calcularIdade para calcular a idade do carro
         carro1.exibirDetalhes();
         carro1.calcularIdade();
     }
