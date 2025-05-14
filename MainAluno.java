e2ca440070656ef21e62f73c71cb822cad640212
vlecabr <vitartasleca@gmail.com>
2025-05-13 23:14:22 -0300

    comentando codigos

--- MainAluno.java
+++ MainAluno.java
@@ -1,11 +1,13 @@
 public class MainAluno {
     public static void main(String[] args) {
+        // Cria um objeto da classe Aluno
         Aluno aluno1 = new Aluno();
 
+        // Atribui valores aos atributos do objeto aluno1
         aluno1.nome = "Victor";
         aluno1.idade = 22;
 
-
+        // Exibe os detalhes do aluno
         aluno1.exibirDetalhes();
     }
 }
